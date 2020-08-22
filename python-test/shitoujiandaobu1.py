40809335d7d01d05e2e494917c2010194de3eebe
chenghongyu666666 <1581300295@qq.com>
2020-08-22 23:04:29 +0800

    print information about the game

--- python-test/shitoujiandaobu1.py
+++ python-test/shitoujiandaobu1.py
@@ -2,3 +2,9 @@ import random
 time = 1
 win = 0
 while win < 4:
+    user = input("请输入对应数字，石头(1)/剪刀(2)/布(3)：")
+    computer = ["1", "2", "3"]
+    computerChoose = random.choice(computer)
+    print("当前是第" + str(time) + "局")
+    print("您出的是:", user)
+    print("电脑出的是:",  computerChoose)
