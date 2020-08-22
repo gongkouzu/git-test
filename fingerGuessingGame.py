f08860c54071047a1c74638230b0e69a137272d2
XiaoYao <xiaoyaoswjtu@163.com>
2020-08-22 20:23:49 +0800

    two side punches

--- fingerGuessingGame.py
+++ fingerGuessingGame.py
@@ -8,3 +8,8 @@ while n < 3:
     k += 1
     print('当前第' + str(k) + '局游戏')
     myNumber = input('请输入猜拳对应数字：')
+    myNumber = int(myNumber)
+    fingerGuessingGame = ['石头', '剪刀', '布']
+    print('你出了' + fingerGuessingGame[myNumber])
+    computerNumbers = random.randint(0, 2)
+    print('电脑出了' + fingerGuessingGame[computerNumbers])
