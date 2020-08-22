60a904eb402774f29bca40b136566ef848002b2c
chenghongyu666666 <1581300295@qq.com>
2020-08-22 23:05:24 +0800

    judgement

--- python-test/shitoujiandaobu1.py
+++ python-test/shitoujiandaobu1.py
@@ -8,3 +8,11 @@ while win < 4:
     print("当前是第" + str(time) + "局")
     print("您出的是:", user)
     print("电脑出的是:",  computerChoose)
+    if (int(user) == 1 and  computerChoose == 2) or (int(user) == 2 and  computerChoose == 3) or (int(user) == 3 and  computerChoose == 1):
+        print('你赢了')
+        win += 1
+    elif int(user) == computer:
+        print("平局")
+    else:
+        print("电脑胜出")
+    print("您已获胜" + str(win)+"局")
