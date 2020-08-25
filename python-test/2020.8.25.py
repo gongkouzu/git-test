b875e2f26a6e1d75aee4946b7e585b86b69c8b86
chenghongyu666666 <1581300295@qq.com>
2020-08-25 22:13:53 +0800

    learn time

---
+++ python-test/2020.8.25.py
@@ -0,0 +1,12 @@
+import time
+print('七夕节干嘛')
+time.sleep(2)
+print('请在以下三个选项【1 啪啪啪 ；2 嘿嘿嘿；3 哈哈哈 】中，选择你最想获得的宝石吧！')
+time.sleep(4)
+anwser = input("请在这后面输入数字:")
+if anwser == "1":
+    print("牛逼")
+elif anwser == "2":
+    print("憨批")
+else:
+    print("煞笔")
